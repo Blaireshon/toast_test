731b49f72aa75bb01aca21aced4d5ed64d033e39
Blaireshon <sonsy09@naver.com>
2024-02-23 16:54:32 +0900

    test 버튼 만듬

--- lib/main.dart
+++ lib/main.dart
@@ -32,6 +32,14 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+  int _counter = 0;
+
+  ///숫자 증가
+  void _incrementCounter() {
+    _counter = ToastView().addOne(_counter);
+    setState(() {});
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -39,57 +47,195 @@ class _MyHomePageState extends State<MyHomePage> {
           backgroundColor: Theme.of(context).colorScheme.inversePrimary,
           title: Text(widget.title),
         ),
-        body: Column(children: [
-          Center(
-            child: Container(
-              width: 200,
-              child: ElevatedButton(
-                onPressed: () {
-                  _showToast('position', context);
-                },
-                style: ElevatedButton.styleFrom(
-                  backgroundColor: Color(0xffFCAF17),
-                  elevation: 0,
+        body: Center(
+          child: Column(
+            children: [
+              Container(
+                width: 200,
+                child: ElevatedButton(
+                  onPressed: () {
+                    _topToast('top position', context);
+                  },
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: Color(0xffFCAF17),
+                    elevation: 0,
+                  ),
+                  child: const Text('top position',
+                      style: TextStyle(color: Colors.white)),
                 ),
-                child: const Text('position',
-                    style: TextStyle(color: Colors.white)),
               ),
-            ),
-          )
-        ]) // This trailing comma makes auto-formatting nicer for build methods.
+              SizedBox(height: 10,),
+              Container(
+                width: 200,
+                child: ElevatedButton(
+                  onPressed: () {
+                    _centerToast('center position', context);
+                  },
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: Color(0xffFCAF17),
+                    elevation: 0,
+                  ),
+                  child: const Text('center position',
+                      style: TextStyle(color: Colors.white)),
+                ),
+              ),
+              SizedBox(height: 10,),
+              Container(
+                width: 200,
+                child: ElevatedButton(
+                  onPressed: () {
+                    _bottomToast('bottom position', context);
+                  },
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: Color(0xffFCAF17),
+                    elevation: 0,
+                  ),
+                  child: const Text('bottom position',
+                      style: TextStyle(color: Colors.white)),
+                ),
+              ),
+              SizedBox(height: 10,),
+              Container(
+                width: 200,
+                child: ElevatedButton(
+                  onPressed: () {
+                    _customPositionToast('custom position', context);
+                  },
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: Color(0xffFCAF17),
+                    elevation: 0,
+                  ),
+                  child: const Text('custom position',
+                      style: TextStyle(color: Colors.white)),
+                ),
+              ),
+              SizedBox(height: 10,),
+              Container(
+                width: 200,
+                child: ElevatedButton(
+                  onPressed: () {
+                    _topAnimationToast('top animation', context);
+                  },
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: Colors.deepOrangeAccent,
+                    elevation: 0,
+                  ),
+                  child: const Text('top animation',
+                      style: TextStyle(color: Colors.white)),
+                ),
+              ),SizedBox(height: 10,),
+              Container(
+                width: 200,
+                child: ElevatedButton(
+                  onPressed: () {
+                    _fadeAnimationToast('fade animation', context);
+                  },
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: Colors.deepOrangeAccent,
+                    elevation: 0,
+                  ),
+                  child: const Text('fade animation',
+                      style: TextStyle(color: Colors.white)),
+                ),
+              ),
+              SizedBox(height: 10,),
+              Container(
+                width: 200,
+                child: ElevatedButton(
+                  onPressed: () {
+                    _incrementCounter();
+                    _layerToast('$_counter', context);
+                  },
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: Colors.green,
+                    elevation: 0,
+                  ),
+                  child: const Text('LAYER',
+                      style: TextStyle(color: Colors.white)),
+                ),
+              ),
+              SizedBox(height: 10,),
+              Container(
+                width: 200,
+                child: ElevatedButton(
+                  onPressed: () {
+                    _incrementCounter();
+                    _normalToast('$_counter', context);
+                  },
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: Colors.green,
+                    elevation: 0,
+                  ),
+                  child: const Text('NORMAL',
+                      style: TextStyle(color: Colors.white)),
+                ),
+              ),
+              SizedBox(height: 10,),
+              Container(
+                width: 200,
+                child: ElevatedButton(
+                  onPressed: () {
+                     _incrementCounter();
+                     _stackToast('$_counter', context);
+                  },
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: Colors.green,
+                    elevation: 0,
+                  ),
+                  child: const Text('STACK',
+                      style: TextStyle(color: Colors.white)),
+                ),
+              ),
+              SizedBox(height: 10,),
+              Container(
+                width: 200,
+                child: ElevatedButton(
+                  onPressed: () {
+                     _shortToast('short toast', context);
+                  },
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: Colors.blue,
+                    elevation: 0,
+                  ),
+                  child: const Text('short toast',
+                      style: TextStyle(color: Colors.white)),
+                ),
+              ),
+              SizedBox(height: 10,),
+              Container(
+                width: 200,
+                child: ElevatedButton(
+                  onPressed: () {
+                     _longToast('long toast', context);
+                  },
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: Colors.blue,
+                    elevation: 0,
+                  ),
+                  child: const Text('long toast',
+                      style: TextStyle(color: Colors.white)),
+                ),
+              ),
+
+            ],
+          ),
+        )// This trailing comma makes auto-formatting nicer for build methods.
         );
   }
 
   ///toast 실행
-  void _showToast(String msg, BuildContext context) {
+  void _topToast(String msg, BuildContext context) {
     /// 객체 선언(인스턴스)
     final toastView = ToastView();
 
-    ///child 위젯 설정
-    Widget toast = Container(
-      height: 50,
-      width: 300,
-      decoration: BoxDecoration(
-        borderRadius: BorderRadius.circular(10.0),
-        color: const Color(0xff67CC36),
-      ),
-      child: Center(
-          child: Text(msg,
-              style: const TextStyle(
-                  color: Colors.white,
-                  decoration: TextDecoration.none,
-                  fontSize: 20),
-              textAlign: TextAlign.center)),
-    );
-
     ///toast 생성
     toastView.createToast(
-        child: toast,
+        child: toast(msg),
         context: context,
-        duration: const Duration(seconds: 3), //시간 설정
-        fadeDuration:  const Duration(milliseconds: 350), // 스르륵 사라짐
+        duration: const Duration(milliseconds: 400), //시간 설정
+       //fadeDuration:  const Duration(milliseconds: 350), // 스르륵 사라짐
        //animation: toastAnimation.RIGHT,
-        position: toastPosition.BOTTOM,
+        position: toastPosition.TOP,
       presentation: toastpresentation.LAYER,
 
        // positionBuilder:  (context, child) {
@@ -104,4 +250,153 @@ class _MyHomePageState extends State<MyHomePage> {
        //  }
         );
   }
+
+
+  void _centerToast(String msg, BuildContext context) {
+    final toastView = ToastView();
+
+    toastView.createToast(
+      child: toast(msg),
+      context: context,
+      duration: const Duration(milliseconds: 400), //시간 설정
+      position: toastPosition.CENTER,
+      presentation: toastpresentation.LAYER,
+    );
+  }
+  void _bottomToast(String msg, BuildContext context) {
+    final toastView = ToastView();
+
+    toastView.createToast(
+      child: toast(msg),
+      context: context,
+      duration: const Duration(milliseconds: 400), //시간 설정
+      position: toastPosition.BOTTOM,
+      presentation: toastpresentation.LAYER,
+    );
+  }
+  void _customPositionToast(String msg, BuildContext context) {
+    final toastView = ToastView();
+
+    toastView.createToast(
+      child: toast(msg),
+      context: context,
+      duration: const Duration(milliseconds: 400), //시간 설정
+      //position: toastPosition.BOTTOM,
+      presentation: toastpresentation.LAYER,
+      positionBuilder:  (context, child) {
+         return Stack(
+           alignment: Alignment.center,
+           children: [
+             Positioned(child: child,
+                 top: 200,
+                 left: 50),
+           ],
+         );
+       }
+    );
+  }
+  void _topAnimationToast(String msg, BuildContext context) {
+    final toastView = ToastView();
+
+    toastView.createToast(
+      child: toast(msg),
+      context: context,
+      duration: const Duration(seconds: 3), //시간 설정
+      animation: toastAnimation.TOP,
+      position: toastPosition.BOTTOM,
+      presentation: toastpresentation.LAYER,
+    );
+  }
+  void _fadeAnimationToast(String msg, BuildContext context) {
+    final toastView = ToastView();
+
+    toastView.createToast(
+      child: toast(msg),
+      context: context,
+      duration: const Duration(seconds: 2), //시간 설정
+      animation: toastAnimation.RIGHT,
+      position: toastPosition.CENTER,
+      fadeDuration: Duration(seconds: 1),
+      presentation: toastpresentation.LAYER,
+    );
+  }
+  void _layerToast(String msg, BuildContext context) {
+    final toastView = ToastView();
+
+    toastView.createToast(
+      child: toast(msg),
+      context: context,
+      duration: const Duration(seconds: 2), //시간 설정
+      animation: toastAnimation.LEFT,
+      position: toastPosition.BOTTOM,
+      fadeDuration: Duration(seconds: 1),
+      presentation: toastpresentation.LAYER,
+    );
+  }
+  void _normalToast(String msg, BuildContext context) {
+    final toastView = ToastView();
+
+    toastView.createToast(
+      child: toast(msg),
+      context: context,
+      duration: const Duration(seconds: 2), //시간 설정
+      animation: toastAnimation.LEFT,
+      position: toastPosition.BOTTOM,
+      fadeDuration: Duration(seconds: 1),
+      presentation: toastpresentation.NORMAL,
+    );
+  }
+  void _stackToast(String msg, BuildContext context) {
+    final toastView = ToastView();
+
+    toastView.createToast(
+      child: toast(msg),
+      context: context,
+      presentation: toastpresentation.LIST,
+    );
+  }
+  void _shortToast(String msg, BuildContext context) {
+    final toastView = ToastView();
+
+    toastView.createToast(
+      child: toast(msg),
+      context: context,
+      duration: const Duration(seconds: 2), //시간 설정
+      animation: toastAnimation.LEFT,
+      position: toastPosition.TOP,
+      presentation: toastpresentation.LAYER,
+    );
+  }
+  void _longToast(String msg, BuildContext context) {
+    final toastView = ToastView();
+
+    toastView.createToast(
+      child: toast(msg),
+      context: context,
+      duration: const Duration(seconds: 5), //시간 설정
+      animation: toastAnimation.RIGHT,
+      position: toastPosition.BOTTOM,
+      presentation: toastpresentation.LAYER,
+    );
+  }
+}
+///child 위젯 설정
+Widget toast(String msg) {
+  return Container(
+    height: 50,
+    width: 300,
+    decoration: BoxDecoration(
+      borderRadius: BorderRadius.circular(10.0),
+      border: Border.all(),
+      // color: const Color(0xff67CC36),
+      color: Colors.white,
+    ),
+    child: Center(
+        child: Text(msg,
+            style: const TextStyle(
+                color: Colors.black54,
+                decoration: TextDecoration.none,
+                fontSize: 20),
+            textAlign: TextAlign.center)),
+  );
 }
